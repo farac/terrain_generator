083f675c2793e65239ad2d689fa97144dd707a1c
farac <brunofarac@gmail.com>
2022-12-20 21:46:46 +0100

    changed to numpy, added more sliders

--- helpers.py
+++ helpers.py
@@ -25,91 +25,37 @@ class Helper:
                 return False
         return True
 
-    def normalize_to_bound_ab(x, x_min, x_max, a, b):
-        y = ((b - a) * (x - x_min / x_max - x_min)) + a
-        return int(y)
-    
-    def value_to_height_enum(value):
-        if value < 0.4:
-            return Elevation.Sea
-        elif value < 0.6:
-            return Elevation.Lowlands
-        elif value < 0.8:
-            return Elevation.Plains
-        elif value < 0.9:
-            return Elevation.Hills
-        elif value < 1:
-            return Elevation.Mountains
+    def scale(data):
+        return (data - np.min(data)) / (np.max(data) - np.min(data))
 
-    def value_to_moisture_enum(value):
-        if value < 0.4:
-            return Moisture.Very_dry
-        elif value < 0.6:
-            return Elevation.Dry
-        elif value < 0.8:
-            return Elevation.Temperate
-        elif value < 0.9:
-            return Elevation.Wet
-        elif value < 1:
-            return Elevation.Very_wet
-
-class Generator:
+class Noise_Generator:
 
     def __init__(self, width, height, seed):
         self.width = width
         self.height = height
         self.seed = seed
+    
+    def generate_noise_array(self,
+                             a1_weight, a2_weight, a3_weight, a4_weight, a5_weight,
+                             a1_scale, a2_scale, a3_scale, a4_scale, a5_scale):
+        simplex.seed(self.seed)
+        ix, iy = np.arange(self.width), np.arange(self.height)
 
-    def generate_heightmap(width, height, seed, 
-                           a1_weight, a2_weight, a3_weight, a4_weight, a5_weight,
-                           a1_scale, a2_scale, a3_scale, a4_scale, a5_scale):
-        simplex.seed(seed)
+        ampl1 = a1_weight * Helper.scale(simplex.noise2array(ix / a1_scale, iy / a2_scale))
+        ampl2 = a2_weight * Helper.scale(simplex.noise2array(ix / a2_scale, iy / a2_scale))
+        ampl3 = a3_weight * Helper.scale(simplex.noise2array(ix / a3_scale, iy / a3_scale))
+        ampl4 = a4_weight * Helper.scale(simplex.noise2array(ix / a4_scale, iy / a4_scale))
+        ampl5 = a5_weight * Helper.scale(simplex.noise2array(ix / a5_scale, iy / a5_scale))
 
-        white = ImageColor.getrgb("#ffffff") #0-0.1
-        gray = ImageColor.getrgb("#999999")
-        dark_green = ImageColor.getrgb("#068a06")
-        light_green = ImageColor.getrgb("#2edc2e")
-        idk = ImageColor.getrgb("#acc91a")
-        brown = ImageColor.getrgb("#a96a09")
-        yellow = ImageColor.getrgb("#f3eb3d")
-        light_blue = ImageColor.getrgb("#32dfe2")
-        blue = ImageColor.getrgb("#1bb1e1")
-        dark_blue = ImageColor.getrgb("#0f52a8")
+        noise_array = (ampl1 + ampl2 + ampl3 + ampl4 + ampl5) / (a1_weight + a2_weight + a3_weight + a4_weight + a5_weight)
+        plt.imsave("map.png", noise_array, cmap="gray")
+        
+        return noise_array
 
-        im = Image.new('RGB',(width, height))
-        heightmap = np.empty((height, width), Elevation)
-        for y in range (0, height):
-            for x in range (0, width):
+class Map:
+    def __init__(self, sealevel, moisture_midpoint):
+        self.sealevel = sealevel
+        self.moisture_midpoint = moisture_midpoint
 
-                ampl1 = a1_weight * simplex.noise2(x / a1_scale, y / a1_scale)
-                ampl2 = a2_weight * simplex.noise2 (x / a2_scale, y / a2_scale) 
-                ampl3 = a3_weight * simplex.noise2(x / a3_scale, y / a3_scale)
-                ampl4 = a4_weight * simplex.noise2(x / a4_scale, y / a4_scale)
-                ampl5 = a5_weight * simplex.noise2 (x / a5_scale, y / a5_scale) 
-
-
-                pixel_val = (ampl1 + ampl2 + ampl3 + ampl4 + ampl5) / (a1_weight + a2_weight + a3_weight + a4_weight + a5_weight)
-                #normalize the values to 1-0
-                #ovo nema smisla ako ces im davat vrijednosti i na kraju pretvarat u sliku + temperature
-                #also pribaci ga nazad u numpy impl radi performanci
-                pixel_val = (pixel_val + 1) / (1+1)
-                print(pixel_val)
-                heightmap[y][x] = Helper.value_to_height_enum(pixel_val)
-                print(heightmap[y][x])
-                #if pixel_val < 0.4:
-                    #im.putpixel((x, y), blue)
-                #elif pixel_val < 0.6:
-                    #im.putpixel((x, y), yellow)
-                #elif pixel_val < 0.8:
-                    #im.putpixel((x, y), light_green)
-                #elif pixel_val < 0.9:
-                    #im.putpixel((x, y), dark_green)
-                #elif pixel_val < 1:
-                    #im.putpixel((x, y), white)
-
-
-                #color = int((pixel_val) * 128)
-                #im.putpixel((x, y), color)
-
-        #im = im.resize((width*2, height*2))
-        #im.save("map.png")
+    def create_from_noisemaps(self, height, moisture):
+        
--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import PySimpleGUI as sg
 from PIL import Image
 
-from helpers import Generator, Helper
+from helpers import Noise_Generator, Helper
 
 sg.theme("DarkAmber")
+
+map_parameters_column=[[sg.Text("Height: "), sg.Input(key="-HEIGHT-", size=6, default_text="300"), sg.Text("px")],
+          [sg.Text("Width: "), sg.Input(key="-WIDTH-", size=6, default_text="400"), sg.Text("px")], 
+          [sg.Text("Seed: "), sg.Input(key="-SEED-", size=12, default_text="123456789")]]
+
+height_moist_sliders_column=[[sg.Text("Sealevel:               "), sg.Slider(key="-SEALEVEL-" ,range=(-1,1), resolution=0.01, orientation='h')],
+          [sg.Text("Moisture midpoint: "), sg.Slider(key="-MOISMID-" ,range=(-1,1), resolution=0.01, orientation='h')]]
 
 heightmap_column = [[sg.Text("Heightmap:")],
           [sg.Text("A1 scale: "), sg.Input(key="-SCALE1-", size=3, default_text="60"),
@@ -31,15 +38,15 @@ tempmap_column = [[sg.Text("Moisture map:")],
 
 map_image = sg.Image("test.png")
 layout = [[map_image],      
-          [sg.Text("Height: "), sg.Input(key="-HEIGHT-", size=6, default_text="256"), sg.Text("px")],
-          [sg.Text("Width: "), sg.Input(key="-WIDTH-", size=6, default_text="400"), sg.Text("px")], 
-          [sg.Text("Seed: "), sg.Input(key="-SEED-", size=12, default_text="123456789")],   
+          [sg.Column(map_parameters_column), sg.VSeperator(), sg.Column(height_moist_sliders_column)],
           [sg.Column(heightmap_column), sg.VSeperator(), sg.Column(tempmap_column)],
-          [sg.Button("Generate"), sg.Button("Exit")],
+          [sg.Button("Generate height and moisture noisemaps"), sg.Button("Create map"), sg.Button("Exit")],
           [sg.Text(key="-ERROR-", text_color="red")]]      
 
 window = sg.Window('Map generator', layout)    
 window.move_to_center
+
+heightmap, moistmap = None, None
 
 while True:
     event, values = window.read()
@@ -49,7 +56,7 @@ while True:
     if event == sg.WIN_CLOSED or event == 'Exit':
         break
 
-    if event == 'Generate':
+    if event == 'Generate height and moisture noisemaps':
         if not Helper.check_strings_not_empty((values['-HEIGHT-'], values['-WIDTH-'])):
             Helper.print_error(window, "No dimension can be blank!")
             continue
@@ -58,11 +65,16 @@ while True:
             width = int(values['-WIDTH-'])
             height = int(values['-HEIGHT-'])
             seed = int(values['-SEED-'])
-            a1_scale = int(values['-SCALE1-'])
-            a2_scale = int(values['-SCALE2-'])
-            a3_scale = int(values['-SCALE3-'])
-            a4_scale = int(values['-SCALE4-'])
-            a5_scale = int(values['-SCALE5-'])
+            temp_a1_scale = int(values['-SCALE1-'])
+            temp_a2_scale = int(values['-SCALE2-'])
+            temp_a3_scale = int(values['-SCALE3-'])
+            temp_a4_scale = int(values['-SCALE4-'])
+            temp_a5_scale = int(values['-SCALE5-'])
+            moist_a1_scale = int(values['-M_SCALE1-'])
+            moist_a2_scale = int(values['-M_SCALE2-'])
+            moist_a3_scale = int(values['-M_SCALE3-'])
+            moist_a4_scale = int(values['-M_SCALE4-'])
+            moist_a5_scale = int(values['-M_SCALE5-'])
         except ValueError:
             Helper.print_error(window, "Casting values to int failed, check input!")
             continue
@@ -74,12 +86,26 @@ while True:
         if not (values['-AMPL1-'] or values['-AMPL2-'] or values['-AMPL3-'] or values['-AMPL4-'] or values['-AMPL5-']):
             Helper.print_error(window, "At least one heightmap amplitude weight must not be 0!")
             continue
+        if not (values['-M_AMPL1-'] or values['-M_AMPL2-'] or values['-M_AMPL3-'] or values['-M_AMPL4-'] or values['-M_AMPL5-']):
+            Helper.print_error(window, "At least one moisture map amplitude weight must not be 0!")
+            continue
 
-        Generator.generate_heightmap(width, height, seed, 
-                                     values['-AMPL1-'], values['-AMPL2-'], values['-AMPL3-'],
-                                     values['-AMPL4-'], values['-AMPL5-'],
-                                     a1_scale, a2_scale, a3_scale, a4_scale, a5_scale)
+        generator = Noise_Generator(width, height, seed)
+        heightmap = generator.generate_noise_array(values['-AMPL1-'], values['-AMPL2-'], values['-AMPL3-'],
+                                                   values['-AMPL4-'], values['-AMPL5-'],
+                                                   temp_a1_scale, temp_a2_scale, temp_a3_scale, temp_a4_scale, temp_a5_scale)
+        moistmap = generator.generate_noise_array(values['-M_AMPL1-'], values['-M_AMPL2-'], values['-M_AMPL3-'],
+                                                 values['-M_AMPL4-'], values['-M_AMPL5-'],
+                                                 moist_a1_scale, moist_a2_scale, moist_a3_scale, moist_a4_scale, moist_a5_scale)
+        is_noise_generated = True
+        map_image.update("map.png")
+        print("Generated noise.")
 
-        #map_image.update("map.png")
-        print("Done.")
+    if event == 'Create map':
+        if not heightmap.any() and not moistmap.any():
+            Helper.print_error(window, "Generate noisemaps using other button first!")
+            continue
+
+
+
 window.close()
