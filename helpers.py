c3eba14a0f608f63e087dcf686883e7caa344dbb
farac <brunofarac@gmail.com>
2022-12-24 11:46:54 +0100

    functionality working

--- helpers.py
+++ helpers.py
@@ -6,21 +6,25 @@ import opensimplex as simplex
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as img
+import matplotlib.colors as col
 from PIL import Image, ImageColor
 
 class Colors:
-    Sea = (0,115,184)
+    Sea = "#0073B8"
 
-    Salt_desert = (242,251,199)
-    Grasslands = (154,205,50)
-    Jungle = (107,141,8)
-    Rainforest = (107,122,52)
+    Salt_desert = "#F2FBC7"
+    Grasslands = "#9ACD32"
+    Jungle = "#6B8D08"
+    Rainforest = "#6B7A34"
 
-    Desert = (244,191,30)
-    Forest = (94,184,21)
+    Desert = "#F4BF1E"
+    Forest = "#5EB815"
 
-    Grayrock = (188,188,188)
-    Snow = (243,241,234)
+    Grayrock = "#BCBCBC"
+    Snow = "#F3F1EA"
+
+    map_colors = col.ListedColormap((Sea, Salt_desert, Desert, Grasslands, Forest, Jungle, Rainforest, Grayrock, Snow))
+    Sea_v, Salt_desert_v, Desert_v, Grasslands_v, Forest_v, Jungle_v, Rainforest_v, Grayrock_v, Snow_v = np.linspace(0, 1, 9)
 
 class Helper:
 
@@ -34,7 +38,16 @@ class Helper:
         return True
 
     def scale(data):
-        return (data - np.min(data)) / (np.max(data) - np.min(data))
+        #a=-1, b=1
+        return (-1 - 1) * ((data - np.min(data)) / (np.max(data) - np.min(data))) + 1
+
+    def fetch_random_int():
+        rng = np.random.default_rng()
+        return rng.integers(np.iinfo(np.int32).max)
+    
+    def random_int_from_seed(seed):
+        rng = np.random.default_rng(seed)
+        return rng.integers(np.iinfo(np.int32).max)
 
 class Noise_Generator:
 
@@ -42,23 +55,33 @@ class Noise_Generator:
         self.width = width
         self.height = height
         self.seed = seed
+        self.rng = np.random.default_rng(seed)
+        ##onapravi da se poziva novi unutar generacije 
+        #mozes generirat seedove za njega i tjt, deterministicki je
     
     def generate_noise_array(self,
                              a1_weight, a2_weight, a3_weight, a4_weight, a5_weight,
-                             a1_scale, a2_scale, a3_scale, a4_scale, a5_scale):
-        simplex.seed(self.seed)
+                             a1_scale, a2_scale, a3_scale, a4_scale, a5_scale, is_moisture = False):
+        # if not is_moisture:
+        #     simplex.seed(self.seed)
+        # else:
+        #     simplex.seed(self.seed + 1)
+        simplex.seed(self.rng.integers(np.iinfo(np.int32).max))
         ix, iy = np.arange(self.width), np.arange(self.height)
 
-        ampl1 = a1_weight * Helper.scale(simplex.noise2array(ix / a1_scale, iy / a2_scale))
-        ampl2 = a2_weight * Helper.scale(simplex.noise2array(ix / a2_scale, iy / a2_scale))
-        ampl3 = a3_weight * Helper.scale(simplex.noise2array(ix / a3_scale, iy / a3_scale))
-        ampl4 = a4_weight * Helper.scale(simplex.noise2array(ix / a4_scale, iy / a4_scale))
-        ampl5 = a5_weight * Helper.scale(simplex.noise2array(ix / a5_scale, iy / a5_scale))
+        ampl1 = a1_weight * (simplex.noise2array(ix / a1_scale, iy / a1_scale))
+        ampl2 = a2_weight * (simplex.noise2array(ix / a2_scale, iy / a2_scale))
+        ampl3 = a3_weight * (simplex.noise2array(ix / a3_scale, iy / a3_scale))
+        ampl4 = a4_weight * (simplex.noise2array(ix / a4_scale, iy / a4_scale))
+        ampl5 = a5_weight * (simplex.noise2array(ix / a5_scale, iy / a5_scale))
 
         noise_array = (ampl1 + ampl2 + ampl3 + ampl4 + ampl5) / (a1_weight + a2_weight + a3_weight + a4_weight + a5_weight)
-        plt.imsave("map.png", noise_array, cmap="gray")
+        if not is_moisture:
+            plt.imsave("height.png", noise_array, cmap="gray")
+        else:
+            plt.imsave("moisture.png", noise_array, cmap="gray")
         
-        return noise_array
+        return Helper.scale(noise_array)
 
 class Map:
     def __init__(self, sealevel, moisture_midpoint):
@@ -78,38 +101,28 @@ class Map:
         __, self.Wet, __ = np.linspace(self.Temperate, self.Very_wet, 3)
 
     def populate_map(self, height, moisture):
-        #height = x[0]
-        #moisture = x[1]
-        map_array = np.zeros(height.shape())
-        for h,m in np.nditer([height, moisture]):
+        size = height.shape
+        map_array = np.zeros(size)
+        it = np.nditer([height, moisture], flags=['multi_index'])
+        for h,m in it:
             if h <= self.Sea:
-                return Colors.Sea
+                map_array[it.multi_index] = Colors.Sea_v
             elif h <= self.Lowlands:
-                if m <= self.Dry: return Colors.Salt_desert
-                if m <= self.Temperate: return Colors.Grasslands
-                if m <= self.Wet: return Colors.Jungle
-                if m <= self.Very_wet: return Colors.Rainforest
+                if m <= self.Dry: map_array[it.multi_index] = Colors.Salt_desert_v
+                elif m <= self.Temperate: map_array[it.multi_index] = Colors.Grasslands_v
+                elif m <= self.Wet: map_array[it.multi_index] = Colors.Jungle_v
+                elif m <= self.Very_wet: map_array[it.multi_index] = Colors.Rainforest_v
             elif h <= self.Plains:
-                if m <= self.Dry: return Colors.Desert
-                if m <= self.Temperate: return Colors.Forest
-                if m <= self.Wet: return Colors.Jungle
-                if m <= self.Very_wet: return Colors.Rainforest
+                if m <= self.Dry: map_array[it.multi_index] = Colors.Desert_v
+                elif m <= self.Temperate: map_array[it.multi_index] = Colors.Forest_v
+                elif m <= self.Wet: map_array[it.multi_index] = Colors.Jungle_v
+                elif m <= self.Very_wet: map_array[it.multi_index] = Colors.Rainforest_v
             elif h <= self.Hills:
-                if m <= self.Dry: return Colors.Grayrock
-                if m <= self.Temperate: return Colors.Forest
-                if m <= self.Wet: return Colors.Jungle
-                if m <= self.Very_wet: return Colors.Rainforest
+                if m <= self.Dry: map_array[it.multi_index] = Colors.Grayrock_v
+                elif m <= self.Temperate: map_array[it.multi_index] = Colors.Forest_v
+                elif m <= self.Wet: map_array[it.multi_index] = Colors.Jungle_v
+                elif m <= self.Very_wet: map_array[it.multi_index] = Colors.Rainforest_v
             elif h <= self.Mountains:
-                if m <= self.Dry: return Colors.Grayrock
-                if m <= self.Wet: return Colors.Snow
+                if m <= self.Dry: map_array[it.multi_index] = Colors.Grayrock_v
+                elif m <= self.Wet: map_array[it.multi_index] = Colors.Snow_v
         return map_array
-
-    # def create_from_noisemaps(self, heightmap, moisturemap):
-    #     #ne ovako ovo je iteracija oba arraya istovremeno, i spremanje u treci
-    #     #nova matrica u koju spremas rezultat bla bla
-    #     for height_val in np.nditer(heightmap):
-    #         for moisture_val in np.nditer(moisturemap):
-    #             if(height_val < self.Sea):
-
-
-
--- main.py
+++ main.py
@@ -1,45 +1,51 @@
 import PySimpleGUI as sg
 from PIL import Image
-
-from helpers import Noise_Generator, Helper, Map
+import matplotlib.pyplot as plt
+from helpers import Noise_Generator, Helper, Map, Colors
 
 sg.theme("DarkAmber")
 
 map_parameters_column=[[sg.Text("Height: "), sg.Input(key="-HEIGHT-", size=6, default_text="300"), sg.Text("px")],
           [sg.Text("Width: "), sg.Input(key="-WIDTH-", size=6, default_text="400"), sg.Text("px")], 
-          [sg.Text("Seed: "), sg.Input(key="-SEED-", size=12, default_text="123456789")]]
+          [sg.Text("Seed: "), sg.Input(key="-SEED-", size=12, default_text=Helper.fetch_random_int())]]
 
-height_moist_sliders_column=[[sg.Text("Sealevel:               "), sg.Slider(key="-SEALEVEL-" ,range=(-1,1), resolution=0.01, orientation='h')],
-          [sg.Text("Moisture midpoint: "), sg.Slider(key="-MOISMID-" ,range=(-1,1), resolution=0.01, orientation='h')]]
+height_moist_sliders_column=[[sg.Text("Sealevel:               "), sg.Slider(key="-SEALEVEL-" ,range=(-1,1), resolution=0.01, orientation='h',default_value=-0.25)],
+          [sg.Text("Moisture midpoint: "), sg.Slider(key="-MOISMID-" ,range=(-1,1), resolution=0.01, orientation='h', default_value=0)]]
 
 heightmap_column = [[sg.Text("Heightmap:")],
-          [sg.Text("A1 scale: "), sg.Input(key="-SCALE1-", size=3, default_text="60"),
-           sg.Text("A1 weight: "), sg.Slider(key="-AMPL1-" ,range=(0,2), resolution=0.01, orientation='h')],
-          [sg.Text("A2 scale: "), sg.Input(key="-SCALE2-", size=3, default_text="50"),
-           sg.Text("A2 weight: "), sg.Slider(key="-AMPL2-" ,range=(0,2), resolution=0.01, orientation='h')],
-          [sg.Text("A3 scale: "), sg.Input(key="-SCALE3-", size=3, default_text="40"),
-           sg.Text("A3 weight: "), sg.Slider(key="-AMPL3-" ,range=(0,2), resolution=0.01, orientation='h')],
-          [sg.Text("A4 scale: "), sg.Input(key="-SCALE4-", size=3, default_text="20"),
-           sg.Text("A4 weight: "), sg.Slider(key="-AMPL4-" ,range=(0,2), resolution=0.01, orientation='h')],
-          [sg.Text("A5 scale: "), sg.Input(key="-SCALE5-", size=3, default_text="10"),
-           sg.Text("A5 weight: "), sg.Slider(key="-AMPL5-" ,range=(0,2), resolution=0.01, orientation='h')]]
+          [sg.Text("A1 scale: "), sg.Input(key="-SCALE1-", size=3, default_text="65"),
+           sg.Text("A1 weight: "), sg.Slider(key="-AMPL1-" ,range=(0,2), resolution=0.01, orientation='h', default_value=2)],
+          [sg.Text("A2 scale: "), sg.Input(key="-SCALE2-", size=3, default_text="55"),
+           sg.Text("A2 weight: "), sg.Slider(key="-AMPL2-" ,range=(0,2), resolution=0.01, orientation='h', default_value=1.6)],
+          [sg.Text("A3 scale: "), sg.Input(key="-SCALE3-", size=3, default_text="45"),
+           sg.Text("A3 weight: "), sg.Slider(key="-AMPL3-" ,range=(0,2), resolution=0.01, orientation='h', default_value=1.2)],
+          [sg.Text("A4 scale: "), sg.Input(key="-SCALE4-", size=3, default_text="25"),
+           sg.Text("A4 weight: "), sg.Slider(key="-AMPL4-" ,range=(0,2), resolution=0.01, orientation='h', default_value=0.8)],
+          [sg.Text("A5 scale: "), sg.Input(key="-SCALE5-", size=3, default_text="15"),
+           sg.Text("A5 weight: "), sg.Slider(key="-AMPL5-" ,range=(0,2), resolution=0.01, orientation='h', default_value=0.4)]]
 
 tempmap_column = [[sg.Text("Moisture map:")],
           [sg.Text("A1 scale: "), sg.Input(key="-M_SCALE1-", size=3, default_text="60"),
-           sg.Text("A1 weight: "), sg.Slider(key="-M_AMPL1-" ,range=(0,2), resolution=0.01, orientation='h')],
+           sg.Text("A1 weight: "), sg.Slider(key="-M_AMPL1-" ,range=(0,2), resolution=0.01, orientation='h', default_value=2)],
           [sg.Text("A2 scale: "), sg.Input(key="-M_SCALE2-", size=3, default_text="50"),
-           sg.Text("A2 weight: "), sg.Slider(key="-M_AMPL2-" ,range=(0,2), resolution=0.01, orientation='h')],
+           sg.Text("A2 weight: "), sg.Slider(key="-M_AMPL2-" ,range=(0,2), resolution=0.01, orientation='h', default_value=1.6)],
           [sg.Text("A3 scale: "), sg.Input(key="-M_SCALE3-", size=3, default_text="40"),
-           sg.Text("A3 weight: "), sg.Slider(key="-M_AMPL3-" ,range=(0,2), resolution=0.01, orientation='h')],
+           sg.Text("A3 weight: "), sg.Slider(key="-M_AMPL3-" ,range=(0,2), resolution=0.01, orientation='h', default_value=1.2)],
           [sg.Text("A4 scale: "), sg.Input(key="-M_SCALE4-", size=3, default_text="20"),
-           sg.Text("A4 weight: "), sg.Slider(key="-M_AMPL4-" ,range=(0,2), resolution=0.01, orientation='h')],
+           sg.Text("A4 weight: "), sg.Slider(key="-M_AMPL4-" ,range=(0,2), resolution=0.01, orientation='h', default_value=0.8)],
           [sg.Text("A5 scale: "), sg.Input(key="-M_SCALE5-", size=3, default_text="10"),
-           sg.Text("A5 weight: "), sg.Slider(key="-M_AMPL5-" ,range=(0,2), resolution=0.01, orientation='h')]]
+           sg.Text("A5 weight: "), sg.Slider(key="-M_AMPL5-" ,range=(0,2), resolution=0.01, orientation='h', default_value=0.4)]]
+
+save_controls =[[sg.Text("Output filename:"), sg.Input(key="-F_PATH-", size=14, default_text="output.png")],
+                [sg.Text("Output image scale factor:"), sg.Input(key="-F_SIZE-", size=3, default_text="2"), sg.Text("times")],
+                [sg.Button("Save map to file")]]
 
 map_image = sg.Image("test.png")
-layout = [[map_image],      
+height_image = sg.Image("test.png")
+moisture_image = sg.Image("test.png")
+layout = [[height_image, moisture_image, map_image],      
           [sg.Column(map_parameters_column), sg.VSeperator(), sg.Column(height_moist_sliders_column)],
-          [sg.Column(heightmap_column), sg.VSeperator(), sg.Column(tempmap_column)],
+          [sg.Column(heightmap_column), sg.VSeperator(), sg.Column(tempmap_column), sg.VSeperator(), sg.Column(save_controls)],
           [sg.Button("Generate height and moisture noisemaps"), sg.Button("Create map"), sg.Button("Exit")],
           [sg.Text(key="-ERROR-", text_color="red")]]      
 
@@ -90,24 +96,49 @@ while True:
             Helper.print_error(window, "At least one moisture map amplitude weight must not be 0!")
             continue
 
+        window['-ERROR-'].update("")
         generator = Noise_Generator(width, height, seed)
         heightmap = generator.generate_noise_array(values['-AMPL1-'], values['-AMPL2-'], values['-AMPL3-'],
                                                    values['-AMPL4-'], values['-AMPL5-'],
                                                    temp_a1_scale, temp_a2_scale, temp_a3_scale, temp_a4_scale, temp_a5_scale)
         moistmap = generator.generate_noise_array(values['-M_AMPL1-'], values['-M_AMPL2-'], values['-M_AMPL3-'],
                                                  values['-M_AMPL4-'], values['-M_AMPL5-'],
-                                                 moist_a1_scale, moist_a2_scale, moist_a3_scale, moist_a4_scale, moist_a5_scale)
-        map_image.update("map.png")
+                                                 moist_a1_scale, moist_a2_scale, moist_a3_scale, moist_a4_scale, moist_a5_scale, True)
+        height_image.update("height.png")
+        moisture_image.update("moisture.png")
         print("Generated noise.")
 
     if event == 'Create map':
-        if not heightmap.any() or not moistmap.any():
+        try: 
+            _ = heightmap.any()
+            _ = moistmap.any()
+        except AttributeError:
             Helper.print_error(window, "Generate noisemaps using other button first!")
             continue
-
+        
+        window['-ERROR-'].update("")
         map_o = Map(values['-SEALEVEL-'], values['-MOISMID-'])
         geomap = map_o.populate_map(heightmap, moistmap)
+        plt.imsave("map.png", geomap, cmap=Colors.map_colors)
+        map_image.update("map.png")
 
+    if event == 'Save map to file':
+        try: 
+            _ = geomap.any()
+        except (AttributeError, NameError):
+            Helper.print_error(window, "No map has been generated!")
+            continue
+        try:
+            factor = int(values["-F_SIZE-"])
+        except ValueError:
+            Helper.print_error(window, "Casting values to int failed, check input!")
+            continue
+        
+        window['-ERROR-'].update("")
+        with Image.open('map.png') as img:
+            (new_width, new_height) = ((img.width * factor, img.height * factor))
+            img_resized = img.resize((new_width, new_height), Image.Resampling.BILINEAR)
+            img_resized.save(values["-F_PATH-"])
 
 
 window.close()
